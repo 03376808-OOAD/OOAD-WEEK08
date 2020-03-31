14befc8967fdda92e5a3f8f3cfcc9a2a6ef4527b
thanaponyalan <thanaponyalan@gmail.com>
2020-03-31 19:25:09 +0700

    delegate with two methods

--- Lab submits/60030037/delegate_act1/delegate_act1/Program.cs
+++ Lab submits/60030037/delegate_act1/delegate_act1/Program.cs
@@ -9,9 +9,15 @@ namespace delegate_act1
         {
             deleHello delH = hello;
             delH("Sawasdee krub.");
+            deleHello delG = goodbye;
+            delG("bye bye.");
         }
 
         public static void hello(string value)
+        {
+            Console.WriteLine(value);
+        }
+        public static void goodbye(string value)
         {
             Console.WriteLine(value);
         }
