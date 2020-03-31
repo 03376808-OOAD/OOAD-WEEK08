265e0752f3e0fb699ef6ec2fd2cb6a2b2970d6c0
thanaponyalan <thanaponyalan@gmail.com>
2020-03-31 19:57:31 +0700

    anonymous method_3 (Lambda Expression)

--- Lab submits/60030037/delegate_act1/delegate_act1/Program.cs
+++ Lab submits/60030037/delegate_act1/delegate_act1/Program.cs
@@ -5,6 +5,7 @@ namespace delegate_act1
     //Anonymous method
 
     delegate int addDel(int a, int b);
+    delegate int xDel(int x);
     class Program
     {
         static void Main(string[] args)
@@ -13,6 +14,9 @@ namespace delegate_act1
                 return a + b;
             };
             Console.WriteLine("result = {0}",adel(2,3));
+
+            xDel x = (int x) => { return x + 3; };
+            Console.WriteLine("Result of xDel = {0}",x(5));
         }
     }
 }
