c9d5700a08bee564a9a21962b5d7a2ae0d2ec9e3
thanaponyalan <thanaponyalan@gmail.com>
2020-03-31 19:48:52 +0700

    normal method

--- Lab submits/60030037/delegate_act1/delegate_act1/Program.cs
+++ Lab submits/60030037/delegate_act1/delegate_act1/Program.cs
@@ -2,44 +2,16 @@
 
 namespace delegate_act1
 {
-    //adding methods to delegate
-    delegate void delMove();
+    //Anonymous method
     class Program
     {
+        public static int add(int a, int b)
+        {
+            return a + b;
+        }
         static void Main(string[] args)
         {
-            delMove L = turnLeft;
-            delMove R = turnRight;
-            delMove gotoSchool = L;
-
-            delMove[] arrDel = new delMove[10];
-            arrDel[0] = L;
-            arrDel[1] = L;
-            arrDel[2] = R;
-            arrDel[3] = L;
-            arrDel[4] = R;
-            arrDel[5] = L;
-            arrDel[6] = R;
-            arrDel[7] = L;
-            arrDel[8] = R;
-            arrDel[9] = L;
-
-            foreach (delMove x in arrDel)
-            {
-                gotoSchool += x;
-            }
-            gotoSchool();
+            Console.WriteLine("result = {0}",add(2,3));
         }
-
-        static public void turnLeft()
-        {
-            Console.WriteLine("Turn Left.");
-        }
-
-        static public void turnRight()
-        {
-            Console.WriteLine("Turn Right.");
-        }
-
     }
 }
