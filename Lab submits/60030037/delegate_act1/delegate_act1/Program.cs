5c1eda3ec4a661ca8820f3c190ef670c245a307a
thanaponyalan <thanaponyalan@gmail.com>
2020-03-31 19:43:58 +0700

    array of delegate

--- Lab submits/60030037/delegate_act1/delegate_act1/Program.cs
+++ Lab submits/60030037/delegate_act1/delegate_act1/Program.cs
@@ -11,13 +11,23 @@ namespace delegate_act1
             delMove L = turnLeft;
             delMove R = turnRight;
             delMove gotoSchool = L;
-            gotoSchool += L;
-            gotoSchool += R;
-            gotoSchool += R;
-            gotoSchool += L;
-            gotoSchool += L;
-            gotoSchool += R;
-            gotoSchool += L;
+
+            delMove[] arrDel = new delMove[10];
+            arrDel[0] = L;
+            arrDel[1] = L;
+            arrDel[2] = R;
+            arrDel[3] = L;
+            arrDel[4] = R;
+            arrDel[5] = L;
+            arrDel[6] = R;
+            arrDel[7] = L;
+            arrDel[8] = R;
+            arrDel[9] = L;
+
+            foreach (delMove x in arrDel)
+            {
+                gotoSchool += x;
+            }
             gotoSchool();
         }
 
