d32b31285b3e280f1db928bf9961fd6174cfbcfc
boaTTTTTT <53853565+boaTTTTTT@users.noreply.github.com>
2020-03-31 16:58:21 +0700

    60030089

---
+++ Lab submits/60030089/delegate_act4/delegate_act4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace delegate_act4
+{
+    delegate int addDel(int a, int b);
+    delegate int xDel(int X);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            addDel adel = (int a, int b) => { return a + b; };
+            Console.WriteLine("result ={0}", adel(2, 3));
+            {
+                xDel x = X => X + 3;
+                Console.WriteLine("result of xDel = {0}", x(5));
+            }
+
+        }
+    }
+}
