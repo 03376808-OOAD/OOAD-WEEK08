37e572111ba038dfe5126efdf6cd719257a0777e
AvirutRut <42264185+AvirutRut@users.noreply.github.com>
2020-03-31 17:01:59 +0700

    finished

---
+++ Lab submits/60030112/delegate 1/delegate_act1/delegate_act1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace delegate_act1
+{
+    delegate void deleHello();
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            deleHello delH = hello;
+            delH();
+        }
+        public static void hello()
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
---
+++ Lab submits/60030112/delegate 2/delegate_act2/delegate_act2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace delegate_act2
+{
+    delegate void deleHello(string value);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            deleHello delH = hello;
+            deleHello delG = goodbye;
+            deleHello delX = delH + delG + delH + delG;
+            delX("ha ha ha.");
+        }
+
+        public static void hello(string value)
+        {
+            Console.WriteLine("Form Hello : {0}",value);
+        }
+
+        public static void goodbye(string value)
+        {
+            Console.WriteLine("Form Goodbye : {0}",value);
+        }
+    }
+}
