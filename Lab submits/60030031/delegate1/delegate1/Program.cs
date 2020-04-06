c63867d9332fd78b1106540f4c7aa7dd10a9dcb1
thanakamon <42264205+thanakamon@users.noreply.github.com>
2020-04-06 21:46:37 +0700

    annonymous method

---
+++ Lab submits/60030031/delegate1/delegate1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace delegate1
+{
+    delegate void deleHello();
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            deleHello delH = hello;
+            delH();
+        }
+
+        public static void hello()
+        {
+            Console.WriteLine("Hello World.");
+        }
+    }
+}
---
+++ Lab submits/60030031/delegate3/delegate3/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace delegate3
+{
+
+    delegate void delMove();
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            delMove L = turnLeft;
+            delMove R = turnRight;
+            delMove gotoSchool = L;
+
+            delMove[] arrdel = new delMove[10];
+            arrdel[0] = L;
+            arrdel[1] = L;
+            arrdel[2] = R;
+            arrdel[3] = L;
+            arrdel[4] = R;
+            arrdel[5] = L;
+            arrdel[6] = R;
+            arrdel[7] = L;
+            arrdel[8] = R;
+            arrdel[9] = L;
+
+
+            foreach (delMove x in arrdel)
+            {
+                gotoSchool += x;
+            }
+            gotoSchool();
+        }
+        static public void turnLeft()
+        {
+            Console.WriteLine("Tern Left.");
+        }
+        static public void turnRight()
+        {
+            Console.WriteLine("Tern Right.");
+        }
+    }
+}
