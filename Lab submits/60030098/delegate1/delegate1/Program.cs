c5e30b93a7309a08ecd7015f1f0d4cac391e3a04
iomxin <42264101+iomxin@users.noreply.github.com>
2020-04-06 21:31:01 +0700

    Add all Pratice

---
+++ Lab submits/60030098/delegate1/delegate1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace delegate1
+{
+    delegate void deleHello();
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            deleHello delH = hello;
+            delH();
+        }
+        public static void hello()
+        {
+            Console.WriteLine("Hello World.");
+        }
+    }
+}
---
+++ Lab submits/60030098/delegate4/delegate4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace delegate4
+{
+    delegate int addDel(int a, int b);
+    delegate int xDel(int x);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            addDel adel = (int a,int b) => { return a + b; };
+            Console.WriteLine("Result = {0}",adel(2,3));
+
+            xDel x = x => x + 3;
+            Console.WriteLine("Result of xDel = {0}",x(5));
+        }
+    }
+}
