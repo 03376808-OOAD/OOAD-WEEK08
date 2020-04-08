8ba5885b469dbda6aa583a943d9722c69ecb7586
thanaponyalan <thanaponyalan@gmail.com>
2020-04-08 20:04:12 +0700

    anonymous method_1

--- Lab submits/60030037/delegate_act4/delegate_act4/Program.cs
+++ Lab submits/60030037/delegate_act4/delegate_act4/Program.cs
@@ -6,13 +6,9 @@ namespace delegate_act4
     delegate int addDel(int a, int b);
     class Program
     {
-        public static int add(int a,int b)
-        {
-            return a + b;
-        }
         static void Main(string[] args)
         {
-            addDel aDel = add;
+            addDel aDel = delegate (int a, int b) { return a + b; };
             Console.WriteLine("result={0}",aDel(2,3));
         }
     }
