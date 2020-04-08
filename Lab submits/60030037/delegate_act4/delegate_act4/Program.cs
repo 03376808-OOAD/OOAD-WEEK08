e4940a86bc2126a526c86adf9f56ef8fbcc0eedd
thanaponyalan <thanaponyalan@gmail.com>
2020-04-08 20:07:17 +0700

    lambda expression

--- Lab submits/60030037/delegate_act4/delegate_act4/Program.cs
+++ Lab submits/60030037/delegate_act4/delegate_act4/Program.cs
@@ -4,12 +4,15 @@ namespace delegate_act4
 {
     //Anonymous method
     delegate int addDel(int a, int b);
+    delegate int xDel(int x);
     class Program
     {
         static void Main(string[] args)
         {
             addDel aDel = (int a, int b) => { return a + b; };
-            Console.WriteLine("result={0}",aDel(2,3));
+            Console.WriteLine("Result = {0}",aDel(2,3));
+            xDel x = (int x) => { return x + 3; };
+            Console.WriteLine("Result of xDel = {0}",x(5));
         }
     }
 }
