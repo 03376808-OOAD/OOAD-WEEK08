594a61f81739975f39862e465902bea931266a6a
thanaponyalan <thanaponyalan@gmail.com>
2020-04-08 19:37:39 +0700

    normal method named hello

---
+++ Lab submits/60030037/delegate_act1/delegate_act1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace delegate_act1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            hello();
+        }
+
+        public static void hello()
+        {
+            Console.WriteLine("Hello World.");
+        }
+    }
+}
