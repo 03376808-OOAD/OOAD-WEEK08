d3a5c5acf5b810ee11503ef08184f0ec9c505e14
thanaponyalan <thanaponyalan@gmail.com>
2020-04-08 20:01:55 +0700

    normal add method

--- Lab submits/60030037/delegate_act4/delegate_act4/Program.cs
+++ Lab submits/60030037/delegate_act4/delegate_act4/Program.cs
@@ -5,9 +5,13 @@ namespace delegate_act4
     //Anonymous method
     class Program
     {
+        public static int add(int a,int b)
+        {
+            return a + b;
+        }
         static void Main(string[] args)
         {
-
+            Console.WriteLine("result={0}",add(2,3));
         }
     }
 }
