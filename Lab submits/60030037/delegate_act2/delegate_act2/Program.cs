466540f620af7be61054a5a8c6a9b78bc4eaac3b
thanaponyalan <thanaponyalan@gmail.com>
2020-04-08 19:41:49 +0700

    delegate with keyword new and shortcut

---
+++ Lab submits/60030037/delegate_act2/delegate_act2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace delegate_act2
+{
+    delegate void deleHello();
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            deleHello delH_new = new deleHello(hello);
+            deleHello delH_shortcut = hello;
+
+            delH_new();
+            delH_shortcut();
+        }
+
+        public static void hello()
+        {
+            Console.WriteLine("Hello World.");
+        }
+    }
+}
