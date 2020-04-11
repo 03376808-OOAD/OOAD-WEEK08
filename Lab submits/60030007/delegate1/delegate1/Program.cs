73d98305f69c5243e7735e9a129143a03f1006ad
gingkasina <42264167+gingkasina@users.noreply.github.com>
2020-04-11 17:34:35 +0700

    1-2

---
+++ Lab submits/60030007/delegate1/delegate1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace delegate1
+{
+    delegate void deleHello(string value);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            deleHello delH = hello;
+            deleHello delG = goodbye;
+            deleHello delX = delH + delG + delH + delG;
+            delX("ha ha ha ");       
+        }
+        public static void hello(string value)
+        {
+            Console.WriteLine("From Hello : {0}", value);
+        }
+        public static void goodbye(string value)
+        {
+            Console.WriteLine("From Goodbye : {0}", value);
+        }
+    }
+}
